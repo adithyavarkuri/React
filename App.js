7e644f8d66f5251feb3f5090861c336972ff9858
adithya <adithya.varkuri@gmail.com>
2024-01-13 19:16:32 +0530

    basic code

--- App.js
+++ App.js
@@ -1,75 +1,44 @@
+import React from "react";
+import ReactDOM from"react-dom/client";
+
+//React Element
+
 const heading  = React.createElement("h1" ,
 {id : "heading" , xyz : "abc"}, "Helloworld from react from app");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)    
 
+const jsxHeading =  <h1 id ="JsxId" className = "jsxclass">This is Jsx heading</h1>
 
-/**
- * <div id = "parent">
- *      <div id = "child">
- *          <h1> I am h1 tag</h1>
- *      </div>
- * </div>
- */
+const jsxHeading1 =  (<h1 id ="JsxId" className = "jsxclass">
+    This is Jsx heading
+</h1>
+)
 
+//root.render(jsxHeading) 
 
-    const parent = React.createElement("div" , {id : "parent"} , 
-    React.createElement("div" , {id : "child"},
-    React.createElement("h1" ,  {}, "I am h1 tag")
-    ))
+//React compnent
+//react functional component
+const  Headingcomponent = () =>{
+    return <h1 id ="JsxId" className = "jsxclass">This is functioanal component</h1>;
+}
+// or write as below
+const  Title = () => <h1 id ="JsxId" className = "jsxclass">This is title component</h1>;
 
-    console.log(parent);
-    root.render(parent);
+//for multiple  
+const  Headingcomponent2 = () =>{
+    return ( <div> 
+             <Title/>
+                <h1 id ="JsxId" className = "jsxclass">This is heading component</h1>
+              </div>);
+}
 
+const number = 100;
+const  Headingcomponent3 = () =>{
+    return ( <div> 
+                {number}
+             <Title/>
+                <h1 id ="JsxId" className = "jsxclass">This is heading component</h1>
+              </div>);
+}
 
-
-    
-/**
- * <div id = "parent1">
- *      <div id = "child1">
- *          <h1> I am h1 tag</h1>
- *          <h2> I am h1 tag</h2>
- *      </div>
- * </div>
- */
-
-
-const parent1 = React.createElement("div" , {id : "parent1"} , 
-    React.createElement("div" , {id : "child1"},
-    [React.createElement("h1" ,  {}, "I am h1 tag") , 
-    React.createElement("h2" ,  {}, "I am h2 tag")]
-    ))
-
-    console.log(parent1);
-    root.render(parent1);
-
-
-/**
- * <div id = "parent2">
- *      <div id = "child2">
- *          <h1> I am h1 tag</h1>
- *          <h2> I am h1 tag</h2>
- *      </div>
- *      <div id = "child2a">
- *          <h1> I am h1 tag</h1>
- *          <h2> I am h1 tag</h2>
- *      </div>
- * </div>
- */
-
-
-const parent2 = React.createElement("div" , {id : "parent1"} , 
-    [React.createElement("div" , {id : "child1"},
-        [React.createElement("h1" ,  {}, "I am h1 tag") , 
-        React.createElement("h2" ,  {}, "I am h2 tag")]
-        ) , 
-    React.createElement("div" , {id : "child2"},
-        [React.createElement("h1" ,  {}, "I am h1 tag") , 
-        React.createElement("h2" ,  {}, "I am h2 tag")]
-        )
-    ]
-);
-
-
-console.log(parent2);
-root.render(parent2);
+root.render(<Headingcomponent3/>)
